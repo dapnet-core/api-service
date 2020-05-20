37e00dd42877525b5245fdbf73a90f8c1d9111c3
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2020-05-20 17:55:49 +0200

    Fix node discovery

--- lib/dapnet/cluster/discovery.ex
+++ lib/dapnet/cluster/discovery.ex
@@ -4,7 +4,7 @@ defmodule Dapnet.Cluster.Discovery do
 
   def nodes, do: GenServer.call(__MODULE__, :nodes)
   def reachable_nodes, do: GenServer.call(__MODULE__, :reachable_nodes)
-  def update, do: GenServer.call(__MODULE__, :update, 10000)
+  def update, do: GenServer.call(__MODULE__, :update)
 
   def start_link do
     GenServer.start_link(__MODULE__, [], [name: __MODULE__])
@@ -23,62 +23,90 @@ defmodule Dapnet.Cluster.Discovery do
 
     Logger.info("Initial node list: #{inspect nodes}")
 
-    {:ok, nodes}
+    {:ok, {nodes, %{}}}
   end
 
-  def handle_call(:update, _from, nodes) do
+  def handle_call(:update, _from, {nodes, requests}) do
     Logger.info("Starting node discovery.")
     body = %{
       name: System.get_env("NODE_NAME"),
       auth_key: System.get_env("NODE_AUTHKEY")
-    } |> Poison.encode!
+    } |> Jason.encode!
 
-    nodes = Task.async_stream(nodes, fn {node, params} ->
+    requests = Enum.map(nodes, fn {node, params} ->
       host = params["host"]
 
-      case HTTPoison.post("#{host}/cluster/discovery", body,
+      %HTTPoison.AsyncResponse{:id => id} = HTTPoison.post!("#{host}/cluster/discovery", body,
         [{"content-type", "application/json"}],
         [
-          recv_timeout: 3000,
-          timeout: 3000
-        ]) do
-        {:ok, response} ->
-          case Poison.decode(response.body) do
-            {:ok, data} -> node_data = Map.get(data, node)
-            if node_data do
-              Logger.info("Reached #{node}!")
-              {node, node_data}
-            else
-              Logger.warn("Could not read response from #{node}!")
-              {node, %{params | "reachable" => false}}
-            end
-            _ ->
-              Logger.warn("Could not decode response from #{node}!")
-              {node, %{params | "reachable" => false}}
+          recv_timeout: 5000,
+          timeout: 5000,
+          stream_to: self()
+        ])
+
+      {id, node}
+    end)
+    |> Map.new()
+    |> Map.merge(requests)
+
+    {:reply, :ok, {nodes, requests}}
+  end
+
+  def handle_info(%HTTPoison.AsyncStatus{}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncHeaders{}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncEnd{:id => id}, {nodes, requests}) do
+    {node, requests} = Map.pop(requests, id)
+    {:noreply, {nodes, requests}}
+  end
+
+  def handle_info(%HTTPoison.Error{:id => id} = e, {nodes, requests}) do
+    {node_name, requests} = Map.pop(requests, id)
+
+    Logger.warn("Could not reach #{node_name}!")
+
+    nodes = Map.update!(nodes, node_name, fn node ->
+      %{node | "reachable" => false}
+    end)
+
+    {:noreply, {nodes, requests}}
+  end
+
+  def handle_info(%HTTPoison.AsyncChunk{"chunk": chunk, "id": id}, {nodes, requests}) do
+    node_name = Map.get(requests, id)
+
+    nodes = if node_name do
+      node = Map.get(nodes, node_name)
+
+      case Jason.decode(chunk) do
+        {:ok, data} ->
+          node_data = Map.get(data, node_name)
+
+          if node_data do
+            Logger.info("Reached #{node_name}!")
+            IO.inspect(node_data)
+            Map.put(nodes, node_name, node_data)
+          else
+            Logger.warn("Could not read response from #{node_name}!")
+            nodes
           end
         _ ->
-          Logger.warn("Could not reach #{node}!")
-          {node, %{params | "reachable" => false}}
+          Logger.warn("Could not decode response from #{node_name}!")
+          nodes
       end
-    end)
-    |> Enum.map(fn {:ok, node} -> node end)
-    |> Map.new()
+    else
+      nodes
+    end
 
-    {:reply, :ok, nodes}
+    {:noreply, {nodes, requests}}
   end
 
-  def handle_info({ref, result}, nodes) when is_reference(ref) do
-    {node, params} = result
-    nodes = Map.put(nodes, node, params)
-    {:noreply, nodes}
-  end
-
-
-  def handle_info({:DOWN, ref, proc, pid, shutdown}, nodes) when is_reference(ref) do
-    {:noreply, nodes}
-  end
-
-  def handle_call(:nodes, _from, nodes) do
+  def handle_call(:nodes, _from, {nodes, _} = state) do
     name = System.get_env("NODE_NAME")
 
     all_nodes = nodes |> Map.put(name, %{
@@ -91,13 +119,13 @@ defmodule Dapnet.Cluster.Discovery do
       }
     })
 
-    {:reply, all_nodes, nodes}
+    {:reply, all_nodes, state}
   end
 
-  def handle_call(:reachable_nodes, _from, nodes) do
+  def handle_call(:reachable_nodes, _from, {nodes, _} = state) do
     reachable_nodes = nodes
     |> Enum.filter(fn {_, params} -> Map.get(params, "reachable") end)
 
-    {:reply, reachable_nodes, nodes}
+    {:reply, reachable_nodes, state}
   end
 end
